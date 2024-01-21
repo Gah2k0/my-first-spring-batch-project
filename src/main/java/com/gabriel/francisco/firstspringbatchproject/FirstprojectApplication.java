bb2b59a23f4e8f943d191cb261c326ea7ea6be80
gah2k0 <gah.francisco@gmail.com>
2024-01-21 15:06:46 -0300

    files

---
+++ src/main/java/com/gabriel/francisco/firstspringbatchproject/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.francisco.firstspringbatchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
